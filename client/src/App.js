b7807168d33c79a9efb85818d4372e06a88ebef3
Jaemin <sjm1013ok@naver.com>
2021-02-24 02:53:31 +0900

    컴포넌트 분리

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-
-const apiUrl = ""
+import Control from './components/Control';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function Home() {
+    var [mode, setMode] = useState();
+    var article = null;
+    if (mode === "LOGIN") {
+        article = <Login />
+    } else if (mode === "REGISTER") {
+        article = <Register />
+    }
     return (
-        <h1>Hello</h1>
+        <>  <h3>Racer Portfolio Service</h3>
+            {article}
+            <Control 
+                onChangeMode = {function(_mode) {
+                    setMode(_mode);
+                }}
+            />
+        </>
     )
 }
 
