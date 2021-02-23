8f83d82ae1c5cd67adc0e45b8b0bc3625c572dc8
Jaemin <sjm1013ok@naver.com>
2021-02-23 20:35:10 +0900

    client(react)와 server(flask) 폴더 분리

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const apiUrl = ""
+
+function Home() {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default Home;
