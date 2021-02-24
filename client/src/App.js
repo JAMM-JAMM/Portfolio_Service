3b72bf2b9eb03041dc99cb2c483615ff2218458f
Jaemin <sjm1013ok@naver.com>
2021-02-24 22:27:52 +0900

    Home 화면 구성 update

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Control from './components/Control';
 import Login from './components/Login';
 import Register from './components/Register';
+import { Container } from 'react-bootstrap';
 
 function Home() {
-    var [mode, setMode] = useState();
+    var [mode, setMode] = useState("HOME");
     var article = null;
     if (mode === "LOGIN") {
         article = <Login />
@@ -13,13 +14,18 @@ function Home() {
         article = <Register />
     }
     return (
-        <>  <h3>Racer Portfolio Service</h3>
-            {article}
-            <Control 
-                onChangeMode = {function(_mode) {
-                    setMode(_mode);
-                }}
-            />
+        <>  
+            <Container>
+                <h3>Racer Portfolio Service</h3>
+                <br/>
+                <Control 
+                    onChangeMode = {function(_mode) {
+                        setMode(_mode);
+                    }}
+                />
+                <br/>
+                {article}
+            </Container>
         </>
     )
 }
