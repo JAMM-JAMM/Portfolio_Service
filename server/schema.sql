70dfc8689e369293af2e1ff788637deada546914
Jaemin <sjm1013ok@naver.com>
2021-03-06 04:52:46 +0900

    network api 추가 및 db 수정(fk)

--- server/schema.sql
+++ server/schema.sql
@@ -5,7 +5,7 @@ create table if not exists user(
     password varchar(128) not null,
     primary key(id),
     unique(email)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 create table if not exists education(
     id int not null AUTO_INCREMENT,
     user_email varchar(32) not null,
@@ -13,16 +13,18 @@ create table if not exists education(
     major varchar(32) not null,
     degree varchar(32) not null,
     create_date timestamp default NOW(),
-    primary key(id)
-);
+    primary key(id),
+    foreign key (user_email) references `user` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 create table if not exists awards(
     id int not null AUTO_INCREMENT,
     user_email varchar(32) not null,
     awardName varchar(32) not null,
     awardDesc text not null,
     create_date timestamp default NOW(),
-    primary key(id)
-);
+    primary key(id),
+    foreign key (user_email) references `user` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 create table if not exists project(
     id int not null AUTO_INCREMENT,
     user_email varchar(32) not null,
@@ -31,8 +33,9 @@ create table if not exists project(
     projectStart date not null,
     projectEnd date not null,
     create_date timestamp default NOW(),
-    primary key(id)
-);
+    primary key(id),
+    foreign key (user_email) references `user` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 create table if not exists certificate(
     id int not null AUTO_INCREMENT,
     user_email varchar(32) not null,
@@ -40,5 +43,6 @@ create table if not exists certificate(
     certificateProvider varchar(32) not null,
     certificateIssueDate date not null,
     create_data timestamp default NOW(),
-    primary key(id)
-);
+    primary key(id),
+    foreign key (user_email) references `user` (`email`)s
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
