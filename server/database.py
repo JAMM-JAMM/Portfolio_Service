d702237e7ca16491e1bb42b33747c1dc12913cc0
ShimJaeMin <apvmf32@gmail.com>
2021-06-15 06:21:42 -0700

    mysql db password 추가

--- server/database.py
+++ server/database.py
@@ -1,11 +1,12 @@
 import pymysql
-from config import USER, HOST, PORT, DB, JWT_SECRET_KEY
+from config import USER, HOST, PORT, DB, PASSWORD
 
 db = pymysql.connect(
     user = USER,
     host = HOST,
     port = PORT,
     db = DB,
+    password = PASSWORD,
     charset = 'utf8',
     autocommit = True
 )
